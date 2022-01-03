c4279f38fba4145dbafda1b62d5b55b88473f84c
nerowasnasty <96870098+nerowasnasty@users.noreply.github.com>
2022-01-03 10:22:00 -0300

    Add an option to handle user's input
    Add an option that will be used to verify if the user's input is valid or not

--- options_handler.py
+++ options_handler.py
@@ -12,6 +12,18 @@ def generate_menu(menu_name, options_array):
 	return input(f'choose an option[{len(options_array) + 1}]: ')
 
 
+def input_options(options_array, user_input):
+		try:
+			if (int(user_input) <= len(options_array) + 1):
+				return True
+
+			else:
+				return False
+
+		except:
+				return False
+
+
 """ MAIN FUNCTION """
 
 
@@ -19,6 +31,14 @@ def main():
 	print('first menu: ', generate_menu('main menu', ['show cards', 'search card', 'add card', 'remove card', 'edit card', 'show decks', 'search deck', 'add deck', 'remove deck', 'edit deck']))
 
 	print('second menu: ', generate_menu('Hello, Eliot', ['marijuana', 'mophine', 'heroin']))
+
+	print('input validation with 1:', input_options(['show cards', 'search card', 'add card', 'remove card', 'edit card', 'show decks', 'search deck', 'add deck', 'remove deck', 'edit deck'], '1'))
+
+	print('input validation with a:', input_options(['show cards', 'search card', 'add card', 'remove card', 'edit card', 'show decks', 'search deck', 'add deck', 'remove deck', 'edit deck'], 'a'))
+
+	print('input validation with 11:', input_options(['show cards', 'search card', 'add card', 'remove card', 'edit card', 'show decks', 'search deck', 'add deck', 'remove deck', 'edit deck'], '11'))
+
+	print('input validation with 12:', input_options(['show cards', 'search card', 'add card', 'remove card', 'edit card', 'show decks', 'search deck', 'add deck', 'remove deck', 'edit deck'], '12'))
 
 
 """ runs main if it's not imported """
