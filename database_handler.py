2366ebd8d8fb7af63c4c65daebc035f5cc4fc88c
nerowasnasty <96870098+nerowasnasty@users.noreply.github.com>
2022-01-11 13:58:16 -0300

    Add functions get results and show them

--- database_handler.py
+++ database_handler.py
@@ -1,6 +1,6 @@
 """ IMPORTS """
 
-
+	
 import sqlite3
 
 
@@ -72,17 +72,50 @@ def insert_into_database(con, cur, table_name, columns, table_values_command, ta
 	con.close()
 
 
+def create_fields_command(fields):
+	fields_command = ""
+
+	for key in fields.keys():
+		if (fields_command):
+			fields_command = f"{fields_command} AND {key} = ?"
+
+		else:
+			fields_command = f"{key} = ?"
+
+	return fields_command
+
+
+def search_by_fields(con, cur, table_name, fields_command, fields):
+	cur.execute(f"""SELECT * FROM {table_name} WHERE {fields_command}""", list(fields.values()))
+
+	results = cur.fetchall()
+
+	con.commit()
+	con.close()
+
+	return results
+
+
+def show_results(results):
+	for result in results:
+		print(result)
+
+
+
 """ MAIN """
 
 
 def main():
 	[con, cur] = open_con("nero_was_nasty")
-	create_table(con, cur, "nerowasnasty", create_table_creation_values_command({"name": "text", "age": "INTEGER", "money": "REAL", "picture": "BLOB"}))
-	print(convert_file_to_blob("C:\\Users\\nero\\projects\\quer_voar_industry_baby.mp3"))
-	print(create_table_creation_values_command({"name": "nero", "age": 19, "money": 1305805.35, "picture": convert_file_to_blob("C:\\Users\\nero\\projects\\image.jpg")}))
-	print(create_columns_command({"name": "nero", "age": 19, "money": 1305805.35, "picture": convert_file_to_blob("C:\\Users\\nero\\projects\\image.jpg")}))
+	#create_table(con, cur, "nerowasnasty", create_table_creation_values_command({"name": "text", "age": "INTEGER", "money": "REAL", "picture": "BLOB"}))
+	#print(convert_file_to_blob("C:\\Users\\nero\\projects\\quer_voar_industry_baby.mp3"))
+	#print(create_table_creation_values_command({"name": "nero", "age": 19, "money": 1305805.35, "picture": convert_file_to_blob("C:\\Users\\nero\\projects\\image.jpg")}))
+	#print(create_columns_command({"name": "nero", "age": 19, "money": 1305805.35, "picture": convert_file_to_blob("C:\\Users\\nero\\projects\\image.jpg")}))
+	#[con, cur] = open_con("nero_was_nasty")
+	#insert_into_database(con, cur, "nerowasnasty", create_columns_command({"name": "nero", "age": 19, "money": 1305805.35, "picture": convert_file_to_blob("C:\\Users\\nero\\projects\\image.jpg")}), create_values_command({"name": "nero", "age": 19, "money": 1305805.35, "picture": convert_file_to_blob("C:\\Users\\nero\\projects\\image.jpg")}), ["nero", 19, 1305805.35, convert_file_to_blob("C:\\Users\\nero\\projects\\image.jpg")])
 	[con, cur] = open_con("nero_was_nasty")
-	insert_into_database(con, cur, "nerowasnasty", create_columns_command({"name": "nero", "age": 19, "money": 1305805.35, "picture": convert_file_to_blob("C:\\Users\\nero\\projects\\image.jpg")}), create_values_command({"name": "nero", "age": 19, "money": 1305805.35, "picture": convert_file_to_blob("C:\\Users\\nero\\projects\\image.jpg")}), ["nero", 19, 1305805.35, convert_file_to_blob("C:\\Users\\nero\\projects\\image.jpg")])
+	#print(create_fields_command({"name": "nero", "age": 19}))
+	show_results(search_by_fields(con, cur, "nerowasnasty", create_fields_command({"name": "nero", "age": 19}), {"name": "nero", "age": 19}))
 
 
 """ runs main if it's not imported """
